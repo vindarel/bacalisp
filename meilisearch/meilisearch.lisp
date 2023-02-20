7a90ea0b09b769dd303d71b40553ff3c65b934c5
vindarel <vindarel@mailz.org>
2023-02-21 00:18:22 +0100

    meilisearch: prettier print results

--- meilisearch/meilisearch.lisp
+++ meilisearch/meilisearch.lisp
@@ -11,8 +11,9 @@ https://docs.meilisearch.com/learn/getting_started/quick_start.html#add-document
 Usage:
 
 - start meilisearch
-- set *base-url* or use with-document
-- post-document and search-document.
+- set *base-url*
+- post-document and search-document
+  - set their :document key or use with-document.
 "))
 
 (in-package :meilisearch-test)
@@ -53,13 +54,13 @@ Please use a JSON string, or… TBD." (type-of s)))
 
 (defmethod post-document ((ht hash-table) &key (document *document-name*) (pretty nil))
   "Get a document has a hash-table, transform it to a JSON string and post it to Meilisearch."
-  (let ((s (shasht:write-json* ht :stream nil :pretty pretty)))
+  (let ((s (json:write-json* ht :stream nil :pretty pretty)))
     (%post-document s :document document)))
 
 
 (defun search-document (q &key (document-name *document-name*)
                             (pretty nil)
-                             (keys "title"))
+                            keys)
   "Search for this document.
 Return: a dict, with Meilisearch keys:
 - hits => vector of dicts with keys: id, name
@@ -77,9 +78,31 @@ Return: a dict, with Meilisearch keys:
       (print-hits dict :keys keys)
       dict)))
 
-(defun print-hits (ht &key (keys "title"))
+(defun print-hits (ht &key keys)
   "Print results from this hash-table raw result."
-  (let ((keys (ensure-list keys)))
-    (loop for hit across (access ht "hits")
-       do (loop for key in keys
-             do (format t "~10a: ~70a~&" key (access hit key))))))
+  (loop for hit across (access ht "hits")
+     do (pprint-hit hit :keys keys)
+       (format t "~%")))
+
+(defun pprint-hit (hit &key keys)
+  "Pretty-print this hit (hash-table).
+If a key or a list of keys are given, print only these keys.
+Otherwise, print in the form key: value, one per line."
+  (if keys
+      (let ((keys (ensure-list keys)))
+        (loop for key in keys
+           do (format t "~10a: ~70a~&" key (access hit key))))
+      (pprint-key-values hit)))
+
+(defun pprint-key-values (ht)
+  "Print this HT data in the form key: value, one per line."
+  (loop for key being the :hash-key of ht
+     do (format t "~10a: ~70a~&" key (access ht key))))
+#|
+id        : 334685
+title     : Ma
+overview  : In this modern-day vision of Mother Mary's pilgrimage, a woman crosses the American Southwest playfully deconstructing the woman’s role in a world of roles.
+genres    : #(Drama)
+poster    : https://image.tmdb.org/t/p/w500/gZ32qFZ5zoIEyAe8io7D0yZJxaA.jpg
+release_date: 1441411200
+|#

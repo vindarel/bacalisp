621008232678a1e388eb6132b8f927124e3a5b30
vindarel <vindarel@mailz.org>
2024-12-02 18:24:56 +0100

    AOC 2024-02 part 2

--- advent/2024/day02.lisp
+++ advent/2024/day02.lisp
@@ -60,6 +60,88 @@
 ;; 2
 
 #+ciel
-(part1 (str:from-file *file-input*))
+(format t "solution day2: ~a~&"(part1 (str:from-file *file-input*)))
 ;; 670
 ;; o/
+
+(defun cut-row (row i)
+  (concatenate 'list
+               (subseq row 0 i)
+               (subseq row (1+ i) (length row))))
+
+(defun maybe-safe-row (row &key (recur t))
+  (when (stringp row)
+    (setf row (mapcar #'parse-integer (str:words row))))
+  (loop :for rest :on row  ;; "on" advances on the list one by one.
+        for cur = (car rest)
+        for next = (or (cadr rest)
+                       (return t))
+        for i = 0 then (incf i)
+        with order = nil
+        for current-order = (cond
+                              ((< cur next) :ascending)
+                              ((> cur next) :descending)
+                              (t
+                               ;; try once again.
+                               (if recur
+                                   (return (maybe-safe-row (cut-row row i) :recur nil))
+                                   (return nil))))
+        for step = (abs (- next cur))
+        if (not (<= 1 step 3))
+          do (if recur
+                 (return (or
+                          (maybe-safe-row (cut-row row (max (1- i) 0)) :recur nil)
+                          (maybe-safe-row (cut-row row (max (1+ i) 0)) :recur nil)
+                          (maybe-safe-row (cut-row row i) :recur nil)
+                          ))
+                 (return nil))
+        if (not order)
+          do (setf order current-order)
+        if (not (equal order current-order))
+          do
+             (if recur
+                 (return (or
+                          (maybe-safe-row (cut-row row (max 0 (1+ i))) :recur nil)
+                          (maybe-safe-row (cut-row row (max 0 (1- i))) :recur nil)
+                          (maybe-safe-row (cut-row row i) :recur nil)
+                          ))
+                 (return nil))
+        finally (return t)))
+
+#++
+(progn
+  ;; still safe
+  (assert (maybe-safe-row "7 6 4 2 1"))
+  (assert (maybe-safe-row "8 6 4 4 1"))
+  ;; T
+
+  (maybe-safe-row "1 3 2 4 5")
+  ;; T
+
+  (maybe-safe-row "1 6 5 4")
+  )
+
+;; still unsafe
+#++
+(progn
+  (maybe-safe-row "9 7 6 2 1")
+  ;; NIL
+
+  (maybe-safe-row "1 2 7 8 9")
+  ;; NIL
+  )
+
+(defun part2 (input)
+  (loop for line in (str:lines input)
+    count (maybe-safe-row line)))
+
+#++
+(part2 *input*)
+;; 4
+
+#++
+(part2 (str:from-file *file-input*))
+;; 700 o/
+
+#+ciel
+(format t "solution day2-part2: ~a~&"(part2 (str:from-file *file-input*)))

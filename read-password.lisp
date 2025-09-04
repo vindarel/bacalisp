2e31457cbe2c1ed8c356a75180fb2f8d51a82e54
vindarel <vindarel@mailz.org>
2025-09-04 23:02:07 +0200

    read-password: eval-when, uiop, unwind-protect

--- read-password.lisp
+++ read-password.lisp
@@ -1,11 +1,13 @@
 
 ;; thanks https://github.com/werelax/consomail/blob/master/consomail.lisp
 
-(defun disable-echo ()
-  (sb-ext:run-program "/bin/stty" '("-echo") :input t :output t))
-
 (defun enable-echo ()
-  (sb-ext:run-program "/bin/stty" '("echo") :input t :output t))
+  (uiop:run-program '("/bin/stty" "echo") :input :interactive :output :interactive))
+
+(defun disable-echo ()
+  (uiop:run-program '("/bin/stty" "-echo") :input :interactive :output :interactive))
+  ;; or:
+  ;; (sb-ext:run-program "/bin/stty" '("-echo") :input t :output t))
 
 
 (defun prompt (msg)
@@ -13,14 +15,14 @@
   (force-output)
   (read-line))
 
-
 (defun pass-prompt ()
   (format t "password: ")
   (force-output)
   (disable-echo)
-  (let ((pwd (read-line)))
-    (enable-echo)
-    pwd))
+  (unwind-protect
+      (read-line)
+    (enable-echo)))
 
-(let ((pass (pass-prompt)))
-  (format t "~&I still know the pass was ~s~&" pass))
+(eval-when (:execute)
+  (let ((pass (pass-prompt)))
+    (format t "~&I still know the pass was ~s~&" pass)))

ad932f5a568b3175fe9ec9ceb5474d545cf6bacc
vindarel <vindarel@mailz.org>
2025-12-02 11:29:54 +0100

    AOC day 2 part 2

--- advent/2025/day02.lisp
+++ advent/2025/day02.lisp
@@ -25,64 +25,32 @@
           (b (subseq start (/ len-start 2))))
       (equal a b))))
 
-#+ignore-premature-optimization
-(defun find-symetrical-invalid (start end &aux len-start
-                                            ;; len-end
-                                            i j
-                                            ;; res
-                                            )
-  "If 2220, try 2222: copy START first half."
-  (setf len-start (length start))
-  ;; (setf len-end (length end))
-  (setf i (parse-integer start))
-  (setf j (parse-integer end))
-
-  (when (and (evenp len-start)
-             (not (equal start end)))
-    (let* ((a (subseq start 0 (/ len-start 2)))
-           (next (str:concat a a))
-           (x (parse-integer next)))
-      (when (and (<= x j)
-                 (>= x i))
-        next)))
-  )
+(defvar *part2* nil
+  "when T, use another predicate in find-next-invalids")
 
 (defun find-next-invalids (start end)
   "Try higher numbers… nothing more clever?"
   (loop for x :from (1+ (parse-integer start))
           :to (1- (parse-integer end))
         :for s = (princ-to-string x)
-        :when (simple-invalid-id s)
+        :when (if (not *part2*)
+                  (simple-invalid-id s)
+                  (repeated-sequences s))
           :collect s))
 
 
 ;; (--> find-invalid-ids (string) (or null cons))
-(defun find-invalid-ids (range &aux start end
-                                 ;; len-start
-                                 len-end
-                                 ;; i j
-                                 (res (list))
-                                 )
+(defun find-invalid-ids (range &aux start end len-end (res (list)))
   "range: string-string"
   (check-type range string)
   (let ((ab (str:split "-" range)))
     (setf start (first ab))
-    ;; (setf len-start (length start))
-    ;; (setf i (parse-integer start))
     (setf end (second ab))
-    (setf len-end (length end))
-    ;; (setf j (parse-integer end))
-    )
+    (setf len-end (length end)))
 
-  (if (simple-invalid-id start)
+  (when (simple-invalid-id start)
       ;; simple case like "11"
-      (push start res)
-      ;; duplicate first half of range start.
-      ;; => premature search of clever path, abandoned.
-      ;; (let ((sym (find-symetrical-invalid start end)))
-        ;; (when sym
-          ;; (push sym res)))
-      )
+      (push start res))
 
   (when (simple-invalid-id end)
       (push end res))
@@ -103,9 +71,6 @@
 (fiveam:def-test test-find-invalid-ids ()
   (fiveam:is (equal 2
                     (length (find-invalid-ids "11-22"))))
-
-  ;; (fiveam:is (string= "1188511885" (find-symetrical-invalid "1188511880" "1188511890")))
-  ;; (fiveam:is (string= "222222" (find-symetrical-invalid "222220" "222224")))
 
   (fiveam:is (equal (list "99") (find-next-invalids "95" "115")))
   (fiveam:is (equal (list "99") (find-invalid-ids "95-115")))
@@ -146,19 +111,7 @@
 ;; part 2
 ;;;;;;;;;;;;;;;;;;;
 
-;; I don't like today :D
-
-(defun has-sequence-repeated-twice (s)
-  (cond
-    ((null s)
-     nil)
-    ((= 2 (length s))
-     (equal (subseq s 0 1)
-            (subseq s 2)))
-    ((= 3 (length s))
-     (equal 1 (length (remove-duplicates s))))
-    (t
-     nil)))
+;; I don't like today :(
 
 (defun only-sequences-of (subs s)
   (null (remove-if #'str:blankp (str:split subs s))))
@@ -168,3 +121,30 @@
         for subs = (subseq s 0 i)
         when (only-sequences-of subs s)
           collect subs))
+
+#+testit
+(fiveam:def-test test-part-2-predicate ()
+  (fiveam:is (equal (list "10")
+                    (repeated-sequences "101010")))
+
+  (fiveam:is (equal (list "99" "111") (find-next-invalids "95" "115")))
+
+  (fiveam:is (equal (list "990" "1010") (find-next-invalids "998" "1012")))
+
+  (fiveam:is (equal (list "824824824") (find-next-invalids "824824821" "824824827")))
+  )
+
+(defun part2 (input)
+  (let ((*part2* t))
+    (reduce #'addup-string (flatten
+                            (mapcar #'find-invalid-ids (parse-input input))))))
+
+
+#+ciel
+(part2 (str:from-file *input*))
+
+#+ciel
+(part2 (str:from-file "day02.txt"))
+;; 45814076230 o/
+;; but
+;; in ±20s. Stupid CPU power to the air lol.
